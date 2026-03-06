e398597be99371f348ab87155a1387a3ef9b6ba4
leirisson <103783610+leirisson@users.noreply.github.com>
2026-03-06 01:31:13 -0400

    feat: Implement e-commerce recommendation system with TensorFlow
    - Added ModelTrainingController to handle model training and recommendations.
    - Created ProductController for managing product interactions and purchases.
    - Introduced TFVisorController for visualizing training progress.
    - Developed UserController to manage user data and interactions.
    - Implemented WorkerController to handle web worker communication for model training.
    - Established event handling system with constants and event dispatching.
    - Created services for user and product data management.
    - Developed views for model training, product display, user selection, and past purchases.
    - Added templates for product cards and past purchases.
    - Implemented CSS styles for improved UI/UX.
    - Set up web worker for asynchronous model training and recommendations.

---
+++ 1-introducao/modulo-3/modelo1-ecommerce/parte01-ecommerce-recomendations-with-tensorflow/src/workers/modelTrainingWorker.js
@@ -0,0 +1,112 @@
+import 'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.22.0/dist/tf.min.js';
+import { workerEvents } from '../events/constants.js';
+
+
+// 🔢 Normalize continuous values (price, age) to 0–1 range
+// Why? Keeps all features balanced so no one dominates training
+// Formula: (val - min) / (max - min)
+// Example: price=129.99, minPrice=39.99, maxPrice=199.99 → 0.56
+const normalize = (value, min, max) => (value - min) / ((max - min) || 1)
+
+async function makeContext(catalog, users) {
+    // primeiro pegar todas as idades
+    const ages = users.map(u => u.age);
+    const prices = catalog.map(p => p.price)
+
+    // pegando a maior idade e a menor idade
+    const minAge = Math.min(...ages)
+    const maxAge = Math.max(...ages)
+
+    const minPrice = Math.min(...prices)
+    const maxPrice = Math.max(...prices)
+
+    const colors = [...new Set(catalog.map(p => p.color))]
+    const caterogires = [...new Set(catalog.map(p => p.category))]
+
+    const colorIndex = Object.entries(
+        colors.map((color, index) => {
+            return [color, index]
+        })
+    )
+
+    const categoriesIndex = Object.entries(
+        caterogires.map((category, index) => {
+            return [category, index]
+        })
+    )
+
+    // computar a media de idade dos compradores por produto
+    // ajuda personalizar
+    const midAge = (minAge + maxAge) / 2 // divide por dois pois são dois pesos: menorIdade + MaiorIdade / 2
+    const ageSums = {} // soma das idades
+    const ageCounts = {}
+
+    users.forEach(user => {
+        user.purchases.forEach(product => {
+            ageSums[product.name] = (ageSums[product.name] || 0) + user.age
+            ageCounts[product.name] = (ageCounts[product.name] || 0) + 1
+        })
+    });
+
+    const productAvgAgeNorm = Object.fromEntries(
+        catalog.map(product => {
+            const avg = ageCounts[product.name] ? ageSums[product.name] / ageCounts[product.name] : midAge
+            return [product.name, normalize(avg, minAge, maxAge)]
+        })
+    )
+
+
+    return {
+        catalog,
+        users,
+        colorIndex,
+        categoriesIndex,
+        minAge, 
+        maxAge,
+        minPrice,
+        maxPrice,
+        numcategories: caterogires.length,
+        numColors: colors.length,
+        dimentions: 2 + caterogires.length + colors.length // numero de dimenções 
+    }
+
+
+
+}
+
+
+async function trainModel({ users }) {
+    console.log('Training model with users:', users);
+    postMessage({ type: workerEvents.progressUpdate, progress: { progress: 1 } });
+
+    // =======================
+    // começa aqui
+    const catalog = await (await fetch('../../data/products.json')).json()
+
+
+    // criando o contexto
+    const context = await makeContext(catalog, users)
+    debugger
+    // =============================
+    postMessage({ type: workerEvents.progressUpdate, progress: { progress: 100 } });
+    postMessage({ type: workerEvents.trainingComplete });
+}
+function recommend({ user }) {
+
+    // postMessage({
+    //     type: workerEvents.recommend,
+    //     user,
+    //     recommendations: sortedItems
+    // });
+
+}
+
+const handlers = {
+    [workerEvents.trainModel]: trainModel,
+    [workerEvents.recommend]: recommend,
+};
+
+self.onmessage = e => {
+    const { action, ...data } = e.data;
+    if (handlers[action]) handlers[action](data);
+};
